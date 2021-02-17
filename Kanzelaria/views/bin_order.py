3a76c8fc3cb58d532bbfc742105134e19126cdf8
Kalibri1995 <Kalibri1995@gmail.com>
2021-02-17 10:11:27 +0500

    Реализована форма набора продуктов, создан шаблон корзины

---
+++ Kanzelaria/views/bin_order.py
@@ -0,0 +1,23 @@
+from pyramid.view import view_config
+from pyramid.response import Response
+
+from sqlalchemy.exc import DBAPIError
+
+from .. import models
+
+
+@view_config(route_name='bin', renderer='../templates/bin_order.jinja2')
+def my_view(request):
+    """
+    В реквесте будут данные из чекбоксов по имени check_product, они несут с собой id товаров
+
+    Так же есть поля 'count_of_{product.id}, т.е. нужно собрать сначала айдишники, а потом пройтись по ним и взять
+    из параметров соотв. поля
+
+    Ну и взять из таблицы записи, отдать их на страницу, можно сразу посчитать стоимость, и тут уже переходим
+    к оформлению заказа
+    """
+    return {'project': 'Kanzelaria'}
+
+
+db_err_msg = "Ошибка БД"
--- Kanzelaria/views/default.py
+++ Kanzelaria/views/default.py
@@ -15,17 +15,4 @@ def my_view(request):
     return {'products': products, 'project': 'Kanzelaria'}
 
 
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to initialize your database tables with `alembic`.
-    Check your README.txt for descriptions and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+db_err_msg = "Ошибка БД"
