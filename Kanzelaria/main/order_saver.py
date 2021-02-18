5debcbaa0fa390d3516c5b0383442b7ca114d9bd
r1bus <r1bus@gmail.ru>
2021-02-18 17:49:08 +0500

    Сделан вывод на фронт инфы о заказе

---
+++ Kanzelaria/main/order_saver.py
@@ -0,0 +1,10 @@
+from Kanzelaria.models.Bin import Bin
+
+all_orders = dict()
+
+
+def append_order(request, products: list):
+    if not all_orders.__contains__(request.client_addr):
+        all_orders[request.client_addr] = Bin(request.client_addr, products)
+
+    return all_orders
--- Kanzelaria/views/bin_order.py
+++ Kanzelaria/views/bin_order.py
@@ -4,24 +4,17 @@ from pyramid.response import Response
 from sqlalchemy.exc import DBAPIError
 
 from .. import models
+from ..main.canzelaria_parser import parse_products_info
+from ..main.order_saver import append_order
 
 
 @view_config(route_name='bin', renderer='../templates/bin_order.jinja2')
 def my_view(request):
-    """
-    В реквесте будут данные из чекбоксов по имени check_product, они несут с собой id товаров
+    products = parse_products_info(request)
+    orders = append_order(request, products)
 
-    Так же есть поля 'count_of_{product.id}, т.е. нужно собрать сначала айдишники, а потом пройтись по ним и взять
-    из параметров соотв. поля
-
-    Ну и взять из таблицы записи, отдать их на страницу, можно сразу посчитать стоимость, и тут уже переходим
-    к оформлению заказа
-    """
     try:
         shops = request.dbsession.query(models.Shop).all()
     except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'shops': shops, 'project': 'Kanzelaria'}
-
-
-db_err_msg = "Ошибка БД"
+        return Response("db_err_msg", content_type='text/plain', status=500)
+    return {'bin': orders[request.client_addr], 'shops': shops, 'project': 'Kanzelaria'}
